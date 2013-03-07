22d7b144898ffdc4dcf3588a9a8e9a9fe259acc8
Stephen Clarkson <clarksones@gmail.com>
2013-03-07 12:47:00 -0700

    Updates

--- decisiontree/DecisionTree.java
+++ decisiontree/DecisionTree.java
@@ -6,15 +6,11 @@ import Main.SupervisedLearner;
 public class DecisionTree extends SupervisedLearner {
 
 	private Node _root;
-	private Matrix _features;
-	private Matrix _labels;
 	
 	@Override
 	public void train(Matrix features, Matrix labels) throws Exception {
-		_features = features;
-		_labels = labels;
 		ID3 id3 = new ID3();
-		Node _root = id3.buildTree(features, labels);
+		_root = id3.buildTree(features, labels);
 		_root.dumpDot(features, labels);
 	}
 
@@ -24,10 +20,13 @@ public class DecisionTree extends SupervisedLearner {
 //		for (int i = 0; i < features.length; i++) {
 //			System.out.println("	Feature" + i + ": " + features[i]);
 //		}
-		makeDecision(features);
+		labels[0] = makeDecision(features);
 	}
 	
 	private double makeDecision(double[] features) {
+		Attribute A = _root.getAttribute();
+		double ACol = A.getColumnPositionID();
+		
 		return 0;
 	}
 	
